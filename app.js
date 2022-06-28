6c5540fa82cf695b9259d692f1b7a61d95d0eb2b
Tu Nguyen <s3608114@rmit.edu.vn>
2022-06-29 00:28:51 +0700

    second commit

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
 
 const menus = document.querySelectorAll('.menu li')
 Array.from(menus).map(item => {
@@ -38,15 +18,24 @@ Array.from(menus).map(item => {
     });
 })
 
-const navMbBtn = document.querySelector('.nav-mobile-btn')
-const header = document.querySelector('.header')
-const close = document.querySelector('.close-btn')
-navMbBtn.onclick = () => {  
-  header.classList.add('active')
-}
-close.onclick = () => {
-  header.classList.remove('active')
-}
+const navMbBtn = document.querySelectorAll('.nav-mobile-btn')
+const header = document.querySelectorAll('.header')
+const close = document.querySelectorAll('.close-btn')
+console.log(navMbBtn);
+Array.from(navMbBtn).map((item,index) => {
+  item.onclick = () => {
+    header[index].classList.add('active')
+
+  }
+})
+Array.from(close).map((item,index) => {
+  item.onclick = () => {
+    header[index].classList.add('active')
+
+  }
+})
+
+
 
 
 
